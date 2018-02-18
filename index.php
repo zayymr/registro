f524802c1b6227b59956703bac7d6c2078886161
zayymr <36123730+zayymr@users.noreply.github.com>
2018-02-18 14:27:21 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title> Bienvenida </title>
+</head>
+<body>
+<h2> Organizacion </h2>
+<br>
+<p> </p>
+<h2>Mision</h2>
+<br>
+<p>Formar profesionales, profesores e investigadores de la contaduría, la administración y la informática, que contribuyan al desarrollo económico del país mediante la solución de los problemas prácticos que enfrentan las empresas y las organizaciones, así como realizar investigación orientada a la generación del conocimiento de estas disciplinas; cultivando en su comunidad el espíritu analítico, crítico y reflexivo, y proporcionando las herramientas técnicas que les permitan ser altamente competitivos en los planos nacional e internacional. </p>
+<br>
+<h2>Vision</h2>
+<br>
+<p>Ser el modelo educativo de mayor influencia en el país y en Iberoamérica, reconocido en los niveles nacional e internacional por la calidad y liderazgo de sus profesionales y por su contribución al desarrollo de la investigación en sus disciplinas. Así mismo, debe convertirse en el modelo en el que se formen y eduquen verdaderos líderes que propicien el desarrollo económico de nuestro país en las organizaciones nacionales. </p>
+</body>
+</html>
