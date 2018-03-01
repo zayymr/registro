f97e78fefb6d2d4cc78156131e0e92fbc77bd5d7
zayymr <zairawydks@gmail.com>
2018-02-28 20:42:55 -0600

    Practica 1 Formulario Registro

---
+++ registro.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title> Registro de usuarios </title> 
+<h2>Registro de usuarios</h2>
+<p>Completa el siguiente formulario con los datos solicitados: </p>
+<form action="/my-handling-form-page" method="post">
+    <div>
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" />
+    </div>
+    <div>
+	<label for="apaterno">Apellido Paterno:</label>
+	<input type="text" id="apaterno" />
+    </div>
+    <div>
+	<label for="amaterno">Apellido Materno:</label>
+	<input type="text" id="amaterno" />
+    </div>
+    <div>
+	<label for="tel">Telefono:</label>
+	<input type="text" id="tel" />
+    </div>	
+    <div>
+        <label for="correo">Correo Electronico:</label>
+        <input type="email" id="correo" />
+    </div>
+</form>
+</head>
+</html>
+
