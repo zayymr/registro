d57c05c1f4b0f80d305e692d67b76f6bb45f2760
zayymr <zairawydks@gmail.com>
2018-02-28 20:34:13 -0600

    Practica 1 Index

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<title> Bienvenida </title>
+<head>
+<body>
+<h2> Descripcion </h2>
+<p> A lo largo de su historia y de su vida académica  la  Facultad  de  Contaduría  y  Administración  ha  ocupado  diferentes  sedes,  diversos inmuebles  que  ha  convertido  temporalmente  en su  casa,  espacios  físicos  en  los  que  se  realizan las  actividades  académicas,  docentes,  administrativas,  de  divulgación  y  culturales,  mismos  que al  paso  del  tiempo  y  del  crecimiento  de  la  Facultad  y  de  la  UNAM,  se  han  tenido  que  ampliar,
+modificar  y  rehacer.</p>
+<br>
+<h2> Mision </h2>
+<p>Formar profesionales, profesores e investigadores de la contaduría, la administración y la informática, que contribuyan al desarrollo económico del país mediante la solución de los problemas prácticos que enfrentan las empresas y las organizaciones, así como realizar investigación orientada a la generación del conocimiento de estas disciplinas; cultivando en su comunidad el espíritu analítico, crítico y reflexivo, y proporcionando las herramientas técnicas que les permitan ser altamente competitivos en los planos nacional e internacional.</p>
+<br>
+<h2>Vision</h2>
+<p>Ser el modelo educativo de mayor influencia en el país y en Iberoamérica, reconocido en los niveles nacional e internacional por la calidad y liderazgo de sus profesionales y por su contribución al desarrollo de la investigación en sus disciplinas. Así mismo, debe convertirse en el modelo en el que se formen y eduquen verdaderos líderes que propicien el desarrollo económico de nuestro país en las organizaciones nacionales.</p>
+<br>
+<h2>Valores</h2>
+<p>Las instalaciones actuales, en el sur de la Ciudad Universitaria,  están  formadas  por  dos  conjuntos  arquitectónicos:  principal  y  de  posgrado  e  investigación.  El primero  se  rige  por  un  eje  norte-sur,  en  cuyos  lados están  erigidos  cuatro  edificios  destinados  a  la impartición  de  clases,  que  contienen  un  total  de  68 aulas;  un  edificio  administrativo  donde  se  encuentran las  oficinas  de  la  dirección  y  las  principales  secretarías;  un  edificio  que  alberga  las  oficinas  administrativas.</p>
+<br>
+</body>
+</head>
+</html>
