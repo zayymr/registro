6503bbf9b9914cabb967409e4f054c79ce4d7084
zayymr <zairawydks@gmail.com>
2018-04-02 19:26:06 -0600

    Se agregaron las validaciones

--- alta_registro.php
+++ alta_registro.php
@@ -1,33 +1,103 @@
+<!DOCTYPE HTML>
 <html>
 <head>
 <link rel="stylesheet" type="text/css"  href="css/alta_registro.css">
-<title>Alta de usuarios</title>
+<title>Alta Registro</title>
 </head>
 <body>
-<h2>Registro de usuarios</h2>
-<p>Los datos se registraron correctamente </p>
 <?php
+//Incluir archivo php de conexiòn
+include 'conexion.php';
 
-$con = pg_connect("host=127.0.0.1 port=5432 dbname=bdregistro user=registro_user password=holamundo123");
+//Asignar funcion de conectar a una variable para conectar a la bd
+$conn = conectar();
 
-if (!$con)
-{
-	echo "<center>Problemas con la Conexion a la Base de Datos</center>";
-	exit;
+//Declaraciòn de variable para control de errores con los formularios
+$err = 0;
+
+//Sanitizar los formularios (quitar caracteres especiales o no pertenecientes al tipo de campo)
+$nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+$apaterno = filter_var($_POST['apaterno'], FILTER_SANITIZE_STRING);
+$amaterno = filter_var($_POST['amaterno'], FILTER_SANITIZE_STRING);
+$telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+$correo = filter_var($_POST['correo'], FILTER_SANITIZE_EMAIL);
+
+//Imprimir datos ingresados
+echo "<h2>Registro</h2>Nombre: ".$nombre;
+echo "<br>Apellido paterno: ".$apaterno;
+echo "<br>Apellido materno: ".$amaterno;
+echo "<br>Telefono: ".$telefono;
+echo "<br>Correo: ".$correo."<br><br>";
+
+//Validar formularios
+if ($nombre) {
+if(!preg_match('/^()[A-ZÁÉÍÓÚÜÑa-záéíóúüñ][a-záéíóúüñ]+(\s[A-ZÁÉÍÓÚÜÑ]?[a-záéíóúüñ]+)*$/',$nombre)){
+echo "Error: Nombre invalido<br>";
+$err = 1;
+}
+} else{
+echo "El campo de nombre no puede ir vacio<br>";
+$err = 1;
 }
 
-$query = "INSERT INTO usuarios VALUES
-	('$_POST[id_usuario]','$_POST[nombre]','$_POST[apaterno]','$_POST[tel]')";
+if ($apaterno) {
+if (!preg_match('/^()[A-ZÁÉÍÓÚÜÑa-záéíóúüñ][a-záéíóúüñ]+(\s[A-ZÁÉÍÓÚÜÑ]?[a-záéíóúüñ]+)*$/',$apaterno)) {
+echo "Error: Apellido paterno invalido<br>";
+$err = 1;
+}
+} else{
+echo "El campo de nombre no puede ir vacio<br>";
+$err = 1;
+}
+if ($amaterno) {
+if (!preg_match('/^()[A-ZÁÉÍÓÚÜÑa-záéíóúüñ][a-záéíóúüñ]+(\s[A-ZÁÉÍÓÚÜÑ]?[a-záéíóúüñ]+)*$/',$amaterno)) {
+echo "Error: Apellido materno invalido<br>";
+$err = 1;
+}
+}
+if ($telefono) {
+if (!preg_match('/[0-9]{8,12}/',$telefono)) {
+echo "Error: Telèfono invalido<br>";
+$err = 1;
+}
+} else{
+echo "El campo de telèfono no puede ir vacio<br>";
+$err = 1;
+}
+if ($correo) {
+if (!preg_match('/^[a-zA-Z][a-zA-Z0-9_\-]?(\.?[a-zA-Z0-9_\-])+@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/',$correo)) {
+echo "Error: Correo invalido<br>";
+$err = 1;
+}
+} else{
+echo "El campo de correo no puede ir vacio<br>";
+$err = 1;
+}
 
-pg_query($query);
+//Insertar datos a db si no hubo errores en caso contrario indicar el error
+if ($err == 0) {
+//Asignar query a variable
+$query = ("INSERT INTO usuarios (nombre,apaterno,amaterno,telefono,correo) VALUES ('$nombre','$apaterno','$amaterno','$telefono','$correo')");
+//Ejecutar query llamando la variable de conexiòn a la bd
+$process = pg_query($conn, $query);
+//Informar si la query se ejecuto o no
+if (!$process) {
+echo "<br>No se pudo realizar el registro";
+}
+else {
+echo "<br>El registro se ha realizado exitosamente";
+}
+} else{
+echo "<br>El registro no se puede realizar por que algunos de los datos no son validos, vuelva a intentarlo";
 
-echo "<h4>El id recibido es: </h4>" . $id_usuario = $_POST['id_usuario'] . "<br/>";
-echo "<h4>El nombre recibido es: </h4>" . $nombre = $_POST['nombre'] . "<br/>";
-echo "<h4>El apellido paterno recibido es: </h4>" . $apaterno = $_POST['apaterno'] . "<br/>";
-echo "<h4>El telefono recibido es: </h4>" . $tel = $_POST['tel'] . "<br/>";
+}
+
+//Cerrar la conexion a la bd
+pg_close($conn);
 ?>
-<br/>
-<a href="http://www.registro.unam.mx/registro.php">Volver al formulario</a> 
+<br><br>
+<form action="http://www.registro.unam.mx/registro.php">
+<input type="submit" value="Regresar al formulario" />
+</form>
 </body>
-</head>
 </html>
